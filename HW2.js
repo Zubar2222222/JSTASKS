a6518f318e4e3a8f21e4789ad63434d184eb8ff4
Anzor <urusov_anzor@yahoo.com>
2022-01-25 20:09:27 +0300

    added homework2: - added work with conditions;

---
+++ HW2.js
@@ -0,0 +1,28 @@
+let year = prompt('In What Year Was Founded Microsoft-1987?','');
+if (year == 1987)
+alert('You Are right!');
+if (year == 1987) {
+alert('True');
+alert('You Are Smart!');
+}
+let year = prompt('In What Year Was Founded Microsoft-1987?','');
+if (year == 2015) {
+alert('You Are Smart!');
+}
+else {
+alert('That Is Not Right'); // any different number,than 1987
+}
+let year = prompt('In What Year Was Founded Microsoft-1987?','');
+if ('year<1987') {
+alert('That Is Too Early...');
+} else if('year>1987') {
+alert('That Is Too Late');
+}
+let accesAllowed;
+let age=prompt('How Old Are You?','');
+if (age>31) {
+accesAllowed=true;
+} else {
+accesAllowed=false;
+}
+alert('accesAllowed');
