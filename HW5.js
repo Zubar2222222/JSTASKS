e77b976c7fcc6b2e0bce362faf1201ccb5cfad50
Anzor <urusov_anzor@yahoo.com>
2022-02-05 15:57:08 +0300

    added home work5: - added work with massives;

---
+++ HW5.js
@@ -0,0 +1,12 @@
+let arr = [ 'Mercedes', {
+  name: 'Robert' },
+  true, function()
+  { alert('Good Morning'); } ];
+
+  // get element with index 1 (object) the show the origin
+  alert(arr[1].name);
+
+  //get element with index 3 (function) and run it
+  arr[3] ();
+
+
